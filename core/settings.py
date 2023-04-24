84ec4528771ea310034905076dc276f5ecd405da
hsinyilin <985471zxv@gmail.com>
2023-04-24 14:30:31 +0800

    update models.py

--- core/settings.py
+++ core/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ntub',
+        'USER': 'admin',
+        'PASSWORD': 'ntub11136',
+        'HOST': 'database-1.c8ojbi459vfc.ap-northeast-3.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
